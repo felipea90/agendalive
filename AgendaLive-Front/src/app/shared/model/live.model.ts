9de3ab158d1f36142a547b4de6535179358dec84
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-07-22 16:49:08 -0300

    agenda live

---
+++ AgendaLive-Front/src/app/shared/model/live.model.ts
@@ -0,0 +1,10 @@
+export class Live
+{
+  id!: string;
+  liveName!: string;
+  channelName!: string;
+  liveDate!: string;
+  livetime!: string;
+  liveLink!: string;
+  registrationDate!: string;
+}
